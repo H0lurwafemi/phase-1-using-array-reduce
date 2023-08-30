27c861fa8d9722f44d0d668623a3c73161895052
unknown <stephenoluwafemi52@gmail.comm>
2023-08-30 23:47:41 +0100

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   0,
+// );
+
+// console.log(totalBatteries);
+
+fetch("http://api.open-notify.org/astros.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(`Holy cow! There are ${data["number"]} humans in space.`);
+  });
